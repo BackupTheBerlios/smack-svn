75fd331e95e754b35074a237ca1ef85c9254623e
loki <loki@b94eeb76-cae6-0310-8167-b2262ad92107>
2004-12-02 16:46:58 +0000

    initial import

---
+++ trunk/gui.c
@@ -0,0 +1,172 @@
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+/* Temp GUI ideas for Smack. */
+
+#include <gtk/gtk.h>
+#include <phat/phat.h>
+#include "lo/lo.h"
+#include "gui-drum.h"
+#include "gui.h"
+
+int main(int argc, char* argv[])
+{
+    GtkWidget* window;
+    GtkWidget* box;
+    GtkWidget* vbox;
+    GtkWidget* label;
+    GtkWidget* button;
+    GtkWidget* sep;
+    GtkWidget* drum;
+        
+    gtk_init(&argc, &argv);
+
+    /* setup OSC */
+    lo_address addr = lo_address_new(NULL, NULL);
+    
+    /* main window */
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Smack GUI Proto");
+    gtk_container_set_border_width(GTK_CONTAINER(window), GUI_SPACING);
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+    gtk_window_set_resizable(GTK_WINDOW(window), TRUE);
+    g_signal_connect(G_OBJECT(window), "delete-event",
+		     G_CALLBACK(gtk_main_quit), NULL);
+    
+    /* hold the individual engines */
+    vbox = gtk_vbox_new(FALSE, GUI_SPACING);
+    gtk_container_add(GTK_CONTAINER(window), vbox);
+    gtk_widget_show(vbox);
+    
+    /* TR 808 */ 
+    box = gtk_hbox_new(FALSE, GUI_SPACING);
+    gtk_box_pack_start(GTK_BOX(vbox), box, FALSE, FALSE, 0);
+    gtk_widget_show(box);
+ 
+    label = gtk_label_new("TR-808");
+    gtk_label_set_markup(GTK_LABEL(label), "<big><b> TR-808 </b></big>");   
+    gtk_widget_show(label);
+    gtk_box_pack_start(GTK_BOX(box), label, FALSE, FALSE, 0);
+    
+    button = phat_slider_button_new_with_range(10, 0, 16, 1, "%.0f");
+    phat_slider_button_set_threshold(PHAT_SLIDER_BUTTON(button), GUI_THRESHOLD);
+    gtk_box_pack_end(GTK_BOX(box), button, FALSE, FALSE, 0);
+    gtk_widget_show(button);
+    label = gtk_label_new("Midi Channel:");
+    gtk_box_pack_end(GTK_BOX(box), label, FALSE, FALSE, 0);
+    gtk_widget_show(label);
+
+    /* hbox for each drum */
+    box = gtk_hbox_new(FALSE, GUI_SPACING);
+    gtk_box_pack_start(GTK_BOX(vbox), box, FALSE, FALSE, 0);
+    gtk_widget_show(box);
+
+    /* bass drum, each drum is a widget, with a null terminated list of name / param pairs */
+    drum = gui_drum_new_drum("Bass", 1, addr, "tune", "/smack/bass/tune", "tone", "/smack/bass/tone", "decay","/smack/bass/decay", NULL);
+    gtk_widget_show(drum);
+    gtk_box_pack_start(GTK_BOX(box), drum, FALSE, FALSE, 0);
+
+    /*seperator*/
+    sep = gtk_vseparator_new();
+    gtk_widget_show(sep);
+    gtk_box_pack_start(GTK_BOX(box), sep, FALSE, FALSE, 0);
+    
+    /* snare drum */
+    drum = gui_drum_new_drum("Snare", 2, addr, "snappy", "/smack/snare/snappy", "tone", "/smack/snare/tone", "decay","/smack/snare/decay", "hpf", "/smack/snare/hpf", NULL);
+    gtk_widget_show(drum);
+    gtk_box_pack_start(GTK_BOX(box), drum, FALSE, FALSE, 0);
+
+     /*seperator*/
+    sep = gtk_vseparator_new();
+    gtk_widget_show(sep);
+    gtk_box_pack_start(GTK_BOX(box), sep, FALSE, FALSE, 0);
+    
+    /* tom  */
+    drum = gui_drum_new_drum("Tom", 3, addr, "tune", "/smack/tom/tune", "noise", "/smack/snare/noise", NULL);
+    gtk_widget_show(drum);
+    gtk_box_pack_start(GTK_BOX(box), drum, FALSE, FALSE, 0);
+
+     /*seperator*/
+    sep = gtk_vseparator_new();
+    gtk_widget_show(sep);
+    gtk_box_pack_start(GTK_BOX(box), sep, FALSE, FALSE, 0);
+    
+    /* conga */
+    drum = gui_drum_new_drum("Conga", 4, addr, "tune", "/smack/conga/tune", "noise", "/smack/conga/noise", NULL);
+    gtk_widget_show(drum);
+    gtk_box_pack_start(GTK_BOX(box), drum, FALSE, FALSE, 0);
+
+     /*seperator*/
+    sep = gtk_vseparator_new();
+    gtk_widget_show(sep);
+    gtk_box_pack_start(GTK_BOX(box), sep, FALSE, FALSE, 0);
+
+    /* rimshot */
+    drum = gui_drum_new_drum("Rimshot", 5, addr, "tune", "/smack/rim/tune", "HPF", "/smack/rim/hpf", NULL);
+    gtk_widget_show(drum);
+    gtk_box_pack_start(GTK_BOX(box), drum, FALSE, FALSE, 0);
+
+     /*seperator*/
+    sep = gtk_vseparator_new();
+    gtk_widget_show(sep);
+    gtk_box_pack_start(GTK_BOX(box), sep, FALSE, FALSE, 0);
+
+    
+    /* claves */
+    drum = gui_drum_new_drum("Claves", 6, addr, "tune", "/s/clave/tune", "BPF", "/s/clave/BPF", NULL);
+    gtk_widget_show(drum);
+    gtk_box_pack_start(GTK_BOX(box), drum, FALSE, FALSE, 0);
+
+     /*seperator*/
+    sep = gtk_vseparator_new();
+    gtk_widget_show(sep);
+    gtk_box_pack_start(GTK_BOX(box), sep, FALSE, FALSE, 0);
+    
+    /* clap */
+    drum = gui_drum_new_drum("Clap", 7, addr, "tone", "/s/clap/tone", "snappy", "/s/clap/snappy", "reverb", "/s/clap/reverb", NULL);
+    gtk_widget_show(drum);
+    gtk_box_pack_start(GTK_BOX(box), drum, FALSE, FALSE, 0);
+
+     /*seperator*/
+    sep = gtk_vseparator_new();
+    gtk_widget_show(sep);
+    gtk_box_pack_start(GTK_BOX(box), sep, FALSE, FALSE, 0);
+
+    
+    /* cowbell */
+    drum = gui_drum_new_drum("Cowbell", 8, addr, "tune1", "/smack/cowbell/tune1", "tune2", "/smack/cowbell/tune2", "decay","/smack/cowbell/decay", NULL);
+    gtk_widget_show(drum);
+    gtk_box_pack_start(GTK_BOX(box), drum, FALSE, FALSE, 0);
+
+     /*seperator*/
+    sep = gtk_vseparator_new();
+    gtk_widget_show(sep);
+    gtk_box_pack_start(GTK_BOX(box), sep, FALSE, FALSE, 0);
+
+    
+    /* highhat */
+    drum = gui_drum_new_drum("Highhat", 9, addr, "tune", "/s/hh/tune", "CH decay", "/s/hh/chdecay", "OH decay", "/s/hh/ohdecay", NULL);
+    gtk_widget_show(drum);
+    gtk_box_pack_start(GTK_BOX(box), drum, FALSE, FALSE, 0);
+
+
+    
+
+    gtk_widget_show(window);
+    gtk_main();
+
+    return 0;
+}
---
+++ trunk/gui.h
@@ -0,0 +1,17 @@
+#ifndef __GUI_H__
+#define __GUI_H__
+
+/* borrowed the enum from specimen! */
+
+enum
+{
+    GUI_SPACING = 6, 		/* space between widgets */
+    GUI_INDENT = 18,		/* how much to indent sections by */
+    GUI_SECSPACE = 18, 		/* space between sections */
+    GUI_TITLESPACE = 12,	/* space between a section title and its contents */
+    GUI_TEXTSPACE = 12,		/* space between a label and its control */
+    GUI_BORDERSPACE = 12,	/* space between a border and its guts */
+    GUI_THRESHOLD = 20,         /* threshold used for sliderbuttons */
+};
+
+#endif /* __GUI_H__ */
