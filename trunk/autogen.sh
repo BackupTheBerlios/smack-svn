453df25762a36d9ba972df6600bf4f4bc2002467
loki <loki@b94eeb76-cae6-0310-8167-b2262ad92107>
2006-04-28 01:08:48 +0000

    added new smackmonkey patches from thorwil, and a clean up of cruft

--- trunk/autogen.sh
+++ trunk/autogen.sh
@@ -2,9 +2,6 @@
 
 echo 'Generating necessary files...'
 aclocal
-libtoolize --automake
-#autoheader -Wall
-autoheader
 #automake --gnu --add-missing -Wall
 automake --gnu --add-missing
 #autoconf -Wall
